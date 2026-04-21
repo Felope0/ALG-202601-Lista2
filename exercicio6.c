9d92fc66c4593f40e9c091c738d5c5aa96ed2bdb
Felipe Augusto <lipedagon@gmail.com>
2026-04-21 17:16:16 -0300

    Add files via upload

---
+++ exercicio6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i;
+int soma=0;
+for (i=0; i<=6; i++){
+    soma= soma+(4+i);
+}
+    printf("a soma de todos os numeros 4 do domino eh: %d\n\n", soma);
+    
+  system("PAUSE");	
+  return 0;
+}
